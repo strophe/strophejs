7a6454445727aedfd7ff195c1892a7e592ac2361
Jack Moffitt <jack@metajack.im>
2008-08-24 01:37:59 +0000

    Created basic connection example.

---
+++ examples/basic.js
@@ -0,0 +1,60 @@
+var BOSH_SERVICE = '/xmpp-httpbind'
+var connection = null;
+
+function log(msg) 
+{
+    $('#log').append('<div></div>').append(document.createTextNode(msg));
+}
+
+function rawInput(data)
+{
+    log('RECV: ' + data);
+}
+
+function rawOutput(data)
+{
+    log('SENT: ' + data);
+}
+
+function strophe_log(level, msg) {
+    log('LOG: ' + msg);
+}
+
+function onConnect(status)
+{
+    if (status == Strophe.Status.CONNECTING) {
+	log('Strophe is connecting.');
+    } else if (status == Strophe.Status.CONNFAIL) {
+	log('Strophe failed to connect.');
+	$('#connect').get(0).value = 'connect';
+    } else if (status == Strophe.Status.DISCONNECTING) {
+	log('Strophe is disconnecting.');
+    } else if (status == Strophe.Status.DISCONNECTED) {
+	log('Strophe is disconnected.');
+	$('#connect').get(0).value = 'connect';
+    } else if (status == Strophe.Status.CONNECTED) {
+	log('Strophe is connected.');
+	connection.disconnect();
+    }
+}
+
+$(document).ready(function () {
+    connection = new Strophe.Connection(BOSH_SERVICE);
+    connection.rawInput = rawInput;
+    connection.rawOutput = rawOutput;
+    connection.log = strophe_log;
+
+    $('#connect').bind('click', function () {
+	var button = $('#connect').get(0);
+	if (button.value == 'connect') {
+	    button.value = 'disconnect';
+
+	    connection.connect($('#jid').get(0).value,
+			       $('#pass').get(0).value,
+			       onConnect);
+	} else {
+	    button.value = 'connect';
+	    connection.disconnect();
+	}
+    });
+});
