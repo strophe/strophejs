0444220a6b09fbf922b583a417f7696418b2f0c9
Jack Moffitt <jack@metajack.im>
2009-06-21 23:12:36 -0600

    Clean up tests a bit.

--- tests/tests.js
+++ tests/tests.js
@@ -2,47 +2,27 @@
 
 Strophe.Test = {
     BOSH_URL: "/xmpp-httpbind",
-    XMPP_DOMAIN: 'stanziq.com',
-    DEFAULT_WAIT: 60,
-    DEFAULT_HOLD: 1,
-    DEFAULT_WINDOW: 5,
-    DEFAULT_ROUTE: "xmpp:stanziq.com:5222",
+    XMPP_DOMAIN: 'collecta.com',
     PUBSUB_COMPONENT: "pubsub.stanziq.com",
     _node_name: "", //node name created in connectCallback function
     connection: null, //connection object created in run function
     
     run: function() {
-	
-
 	$(document).ready(function(){
-
 	    //Connect strophe, uses localhost to test
 	    Strophe.Test.connection = new Strophe.Connection(Strophe.Test.BOSH_URL);
-
 	    
 	    //connect anonymously to run most tests
-            var username = Strophe.Test.XMPP_DOMAIN;
-            var password = "password";
-	    
-        
-
-	    Strophe.Test.connection.connect(username,
-			       password,
-			       Strophe.Test.connectCallback,
-                               Strophe.Test.DEFAULT_WAIT,
-                               Strophe.Test.DEFAULT_HOLD,
-                               Strophe.Test.DEFAULT_WINDOW,
-                               Strophe.Test.DEFAULT_ROUTE);
-
+	    Strophe.Test.connection.connect(Strophe.Test.XMPP_DOMAIN,
+                                            null,
+			                    Strophe.Test.connectCallback);
 
 	    //set up the test client UI
 	    $("#disconnect").click(function() {
 		Strophe.Test.connection.disconnect();
 	    });
 	    $("#run_tests").click(function() {      
-
 		test("Anonymous connection test.", function() {
-		    
 		    if(Strophe.Test.connection.connected)
 		    {
 			ok( true, "all good");
@@ -54,109 +34,102 @@ Strophe.Test = {
 		});
 
 		test("Create default node test.",function(){
-		    var iqid =  Strophe.Test.connection.pubsub.createNode(Strophe.Test.connection.jid,
-								   Strophe.Test.PUBSUB_COMPONENT,
-								   Strophe.Test._node_name,{}, function(stanza) {
-
-						  
-			       test("handled create node.",
-				function() {
-				    var error = $(stanza).find("error");
-				
-				    if(error.length == 0)
-				    {
-					ok(true, "returned");
-				    }
-				    else
-				    {
-					ok(false,"error creating node.");
-				    }
-				});
-					      });
+		    var iqid =  Strophe.Test.connection.pubsub
+                        .createNode(Strophe.Test.connection.jid,
+				    Strophe.Test.PUBSUB_COMPONENT,
+				    Strophe.Test._node_name, {}, 
+                                    function(stanza) {
+			                test("handled create node.",
+				             function() {
+				                 var error = $(stanza).find("error");
+				                 
+				                 if(error.length == 0)
+				                 {
+					             ok(true, "returned");
+				                 }
+				                 else
+				                 {
+					             ok(false,"error creating node.");
+				                 }
+				             });
+				    });
 		    ok(true,"sent create request. "+ iqid);
 		});
 
-		
-
-
 		test("subscribe to a node",function() {
-
-
-		    var iqid = Strophe.Test.connection.pubsub.subscribe(Strophe.Test.connection.jid,
-					    Strophe.Test.PUBSUB_COMPONENT,
-					    Strophe.Test._node_name,
-					      [],
-			function(stanza) {
-			    test("items received", function() {
-				console.log(stanza);
-				if($(stanza).length > 0)
-				{
-				    ok(true,"item received.");
-				}
-				else
-				{
-				    ok(false,"no items.");
-				}
-			    });
-			},
-			function(stanza) {
-			    
-			    var error = $(stanza).find("error");
-			    
-			    test("handled subscribe",
-				 function() {
-				if(error.length == 0)
-				    {
-					ok(true,"subscribed");
-				    }
-				else
-				    {
-					console.log(error.get(0));
-					ok(false,
-					   "not subscribed");
-				    }
-				
-			    });
-			});
+		    var iqid = Strophe.Test.connection.pubsub
+                        .subscribe(Strophe.Test.connection.jid,
+				   Strophe.Test.PUBSUB_COMPONENT,
+				   Strophe.Test._node_name,
+				   [],
+			           function(stanza) {
+			               test("items received", function() {
+				           console.log(stanza);
+				           if($(stanza).length > 0)
+				           {
+				               ok(true,"item received.");
+				           }
+				           else
+				           {
+				               ok(false,"no items.");
+				           }
+			               });
+			           },
+			           function(stanza) {
+			               var error = $(stanza).find("error");
+			               
+			               test("handled subscribe",
+				            function() {
+				                if(error.length == 0)
+				                {
+					            ok(true,"subscribed");
+				                }
+				                else
+				                {
+					            console.log(error.get(0));
+					            ok(false,
+					               "not subscribed");
+				                }
+				                
+			                    });
+			           });
 		    
 		    if(iqid)
 			ok(true,
 			   "subscribed to " + Strophe.Test._node_name);
-
 		});
 
 		test("publish to a node",function() {
-
-		    var iqid = Strophe.Test.connection.pubsub.publish(Strophe.Test.connection.jid,
-					  Strophe.Test.PUBSUB_COMPONENT,
-					  Strophe.Test._node_name,
-			{test:'test'},
-					  function(stanza) {
-						
-						var error = $(stanza).find("error");
-						
-						test("handled published item",
-						     function() {
-						    if(error.length == 0)
-						    {
-							ok(true,"got item");
-						    }
-						    else
-						    {
-							ok(false,
-							   "no item");
-						    }
-						    
-						});
-					    });
+		    var iqid = Strophe.Test.connection.pubsub
+                        .publish(Strophe.Test.connection.jid,
+				 Strophe.Test.PUBSUB_COMPONENT,
+				 Strophe.Test._node_name,
+			         {test:'test'},
+				 function(stanza) {
+				     var error = $(stanza).find("error");
+				     
+				     test("handled published item",
+					  function() {
+					      if(error.length == 0)
+					      {
+						  ok(true,"got item");
+					      }
+					      else
+					      {
+						  ok(false,
+						     "no item");
+					      }
+					      
+					  });
+				 });
 		    
 		    if(iqid)
 			ok(true,
 			   "published to " + Strophe.Test._node_name);
-
+                    
 		});		
-		test("subscribe to a node with options",function() {
 
+		test("subscribe to a node with options",function() {
 		    var keyword_elem = Strophe.xmlElement("field",
 							  [["var",
 							    "http://stanziq.com/search#keyword"],
@@ -169,143 +142,147 @@ Strophe.Test = {
 		    value.appendChild(text);
 		    keyword_elem.appendChild(value);
 		    
-		    var iqid = Strophe.Test.connection.pubsub.subscribe(Strophe.Test.connection.jid,
-					    Strophe.Test.PUBSUB_COMPONENT,
-					    Strophe.Test._node_name,
-			[keyword_elem],
-		        function(stanza) {console.log(stanza);},
-			function(stanza) {
-			    
-			    var error = $(stanza).find("error");
-			    
-			    test("handled subscribe with options",
-				 function() {
-				if(error.length == 0)
-				    {
-					ok(true,"search subscribed");
-				    }
-				else
-				    {
-					console.log(error.get(0));
-					ok(false,
-					   "search not subscribed");
-				    }
-				
-			    });
-			});
+		    var iqid = Strophe.Test.connection.pubsub
+                        .subscribe(Strophe.Test.connection.jid,
+				   Strophe.Test.PUBSUB_COMPONENT,
+				   Strophe.Test._node_name,
+			           [keyword_elem],
+		                   function(stanza) {console.log(stanza);},
+			           function(stanza) {
+			               
+			               var error = $(stanza).find("error");
+			               
+			               test("handled subscribe with options",
+				            function() {
+				                if(error.length == 0)
+				                {
+					            ok(true,"search subscribed");
+				                }
+				                else
+				                {
+					            console.log(error.get(0));
+					            ok(false,
+					               "search not subscribed");
+				                }
+				                
+			                    });
+			           });
 		    
 		    if(iqid)
 			ok(true,
 			   "subscribed to search");
 		});
-		test("unsubscribe to a node",function() {
 
-		    var iqid = Strophe.Test.connection.pubsub.unsubscribe(Strophe.Test.connection.jid,
-					      Strophe.Test.PUBSUB_COMPONENT,
-					      Strophe.Test._node_name,
-		       function(stanza) {
-			   
-			   var error = $(stanza).find("error");
-			   
-			   test("handled unsubscribe",
-				function() {
-			       if(error.length == 0)
-				   {
-				       ok(true,"unsubscribed");
-				   }
-			       else
-				   {
-				       console.log(error.get(0));
-				       ok(false,
-					  "unable to unsubscribed");
-				   }
-			       
-			   });
-		       });
+		test("unsubscribe to a node",function() {
+		    var iqid = Strophe.Test.connection.pubsub
+                        .unsubscribe(Strophe.Test.connection.jid,
+				     Strophe.Test.PUBSUB_COMPONENT,
+				     Strophe.Test._node_name,
+		                     function(stanza) {
+			                 var error = $(stanza).find("error");
+			                 
+			                 test("handled unsubscribe",
+				              function() {
+			                          if(error.length == 0)
+				                  {
+				                      ok(true,"unsubscribed");
+				                  }
+			                          else
+				                  {
+				                      console.log(error.get(0));
+				                      ok(false,
+					                 "unable to unsubscribed");
+				                  }
+			                      });
+		                     });
 		    
 		    if(iqid)
 			ok(true,
 			   "unsubscribed from search with no options.");
 		});
-		test("test items retrieval",function(){
-		    var itemid = Strophe.Test.connection.pubsub.items(Strophe.Test.connection.jid,
-					      Strophe.Test.PUBSUB_COMPONENT,
-					      Strophe.Test._node_name,
-				 function(stanza) {
-				     ok(true,"item request successful.");
-				 },
-				 function(stanza) {
-				     ok(false,"failed to send request.");
-				 });
 
+		test("test items retrieval",function(){
+		    var itemid = Strophe.Test.connection.pubsub
+                        .items(Strophe.Test.connection.jid,
+			       Strophe.Test.PUBSUB_COMPONENT,
+			       Strophe.Test._node_name,
+			       function(stanza) {
+				   ok(true,"item request successful.");
+			       },
+			       function(stanza) {
+				   ok(false,"failed to send request.");
+			       });
+                    
 		    if(itemid)
 		    {
 			ok(true,"item request sent.");
 		    }
 		});
+
 		test("test sendIQ interface.",function(){
 		    var sendiq_good = false;
 		    //setup timeout for sendIQ for 3 seconds
 		    setTimeout(function() {
-			ok(sendiq_good, "The iq didn't timeout.");
+                        test("Timeout check", function () {
+			    ok(sendiq_good, "The iq didn't timeout.");
+                        });
 		    }, 3000);
-
+                    
 		    //send a pubsub subscribe stanza
-
-		    var sub = $iq( 
-		              {from:Strophe.Test.connection.jid, 
-			      to:Strophe.Test.PUBSUB_COMPONENT, 
-			      type:'set'}
-		    );
-		    sub.c('pubsub', { xmlns:Strophe.NS.PUBSUB }).c('subscribe',
-		    {node:Strophe.Test._node_name,
-			      jid:Strophe.Test.connection.jid});
+                    
+		    var sub = $iq({from:Strophe.Test.connection.jid,
+			           to:Strophe.Test.PUBSUB_COMPONENT,
+			           type:'set'})
+		        .c('pubsub', { xmlns:Strophe.NS.PUBSUB })
+                        .c('subscribe',
+		           {node:Strophe.Test._node_name,
+			    jid:Strophe.Test.connection.jid});
 		    var stanza=sub.tree();
 		    //call sendIQ with several call backs
-		    Strophe.Test.connection.sendIQ(stanza,
-		    function(stanza) {
-			test("iq sent",function() {
-			    sendiq_good = true;
-			    ok(true,"iq sent succesfully.");
-			});
-		    },
-		    function(stanza) {
-			test("iq fail",function() {
-			    sendiq_good = false;
-			    console.log(stanza);
-			    ok(false,"failed to send iq.");
-			});
-		    });
-
-
+		    Strophe.Test.connection
+                        .sendIQ(stanza, 
+                                function(stanza) {
+			            test("iq sent",function() {
+			                sendiq_good = true;
+			                ok(true,"iq sent succesfully.");
+			            });
+		                },
+		                function(stz) {
+			            test("iq fail",function() {
+                                        if (stz)
+			                    sendiq_good = true;
+			                console.log(stanza);
+			                ok(true,"failed to send iq.");
+			            });
+		                });
 		});
+
 		test("test sendIQ failed.",function(){
-		    var sub = $iq( 
-		    {from:Strophe.Test.connection.jid, 
-			      to:Strophe.Test.PUBSUB_COMPONENT, 
-			      type:'get'}
-		    );
+		    var sub = $iq({from:Strophe.Test.connection.jid, 
+			           to:Strophe.Test.PUBSUB_COMPONENT, 
+			           type:'get'});
+
 		    //call sendIQ with several call backs
-		    
-		    Strophe.Test.connection.sendIQ(sub.tree(),
-		    function(stanza) {
-			console.log(stanza);
-			test("iq sent",function() {
-			    ok(false,
-			       "iq sent succesfully when should have failed.");
-			});
-		    },
-		    function(stanza) {
-			test("iq fail",function() {
-			    ok(true,
-			       "success on failure test: failed to send iq.");
-			});
-		    });		    
+		    Strophe.Test.connection
+                        .sendIQ(sub.tree(),
+		                function(stanza) {
+			            console.log(stanza);
+			            test("iq sent",function() {
+			                ok(false,
+			                   "iq sent succesfully when should have failed.");
+			            });
+		                },
+		                function(stanza) {
+			            test("iq fail",function() {
+			                ok(true,
+			                   "success on failure test: failed to send iq.");
+			            });
+		                });		    
 		});
 	    });
-	    
 	});
     },
+
     connectCallback: function(status,cond) {
 	var error_message = null;
 	if(status == Strophe.Status.CONNECTED)
