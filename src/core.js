e32124ad734a907b4b60ef699648467d98797b46
Jack Moffitt <jack@metajack.im>
2011-05-24 06:29:51 -0600

    Small documentation fix.
    Found by Sonny.

--- src/core.js
+++ src/core.js
@@ -1561,7 +1561,7 @@ Strophe.Connection.prototype = {
      *      or a full JID.  If a node is not supplied, SASL ANONYMOUS
      *      authentication will be attempted.
      *    (String) pass - The user's password.
-     *    (Function) callback The connect callback function.
+     *    (Function) callback - The connect callback function.
      *    (Integer) wait - The optional HTTPBIND wait value.  This is the
      *      time the server will wait before returning an empty result for
      *      a request.  The default setting of 60 seconds is recommended.
