87e6fbe720e6524d7928817a7de5912165d2e065
Gordin <9ordin@googlemail.com>
2012-04-25 15:34:49 +0200

    removed unused variable _connect_callback

--- src/core.js
+++ src/core.js
@@ -1824,8 +1824,7 @@ Strophe.Connection.prototype = {
 
         this._changeConnectStatus(Strophe.Status.CONNECTING, null);
 
-        var _connect_cb = this._connect_callback || this._connect_cb;
-        this._connect_callback = null;
+        var _connect_cb = this._connect_cb;
 
         this._requests.push(
             new Strophe.Request(body.tree(),
