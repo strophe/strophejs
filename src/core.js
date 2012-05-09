9956cfefc95141d0fa0bf029be1575c477f5f8f3
Gordin <9ordin@googlemail.com>
2012-05-09 16:26:04 +0200

    reindented stuff (preparation for next commit)

--- src/core.js
+++ src/core.js
@@ -2851,37 +2851,37 @@ Strophe.Connection.prototype = {
             this.rawInput(Strophe.serialize(bodyWrap));
         }
 
-        var typ = bodyWrap.getAttribute("type");
-        var cond, conflict;
-        if (typ !== null && typ == "terminate") {
-            // an error occurred
-            cond = bodyWrap.getAttribute("condition");
-            conflict = bodyWrap.getElementsByTagName("conflict");
-            if (cond !== null) {
-                if (cond == "remote-stream-error" && conflict.length > 0) {
-                    cond = "conflict";
+            var typ = bodyWrap.getAttribute("type");
+            var cond, conflict;
+            if (typ !== null && typ == "terminate") {
+                // an error occurred
+                cond = bodyWrap.getAttribute("condition");
+                conflict = bodyWrap.getElementsByTagName("conflict");
+                if (cond !== null) {
+                    if (cond == "remote-stream-error" && conflict.length > 0) {
+                        cond = "conflict";
+                    }
+                    this._changeConnectStatus(Strophe.Status.CONNFAIL, cond);
+                } else {
+                    this._changeConnectStatus(Strophe.Status.CONNFAIL, "unknown");
                 }
-                this._changeConnectStatus(Strophe.Status.CONNFAIL, cond);
-            } else {
-                this._changeConnectStatus(Strophe.Status.CONNFAIL, "unknown");
+                return;
             }
-            return;
-        }
 
-        // check to make sure we don't overwrite these if _connect_cb is
-        // called multiple times in the case of missing stream:features
-        if (!this.sid) {
-            this.sid = bodyWrap.getAttribute("sid");
-        }
-        if (!this.stream_id) {
-            this.stream_id = bodyWrap.getAttribute("authid");
-        }
-        var wind = bodyWrap.getAttribute('requests');
-        if (wind) { this.window = parseInt(wind, 10); }
-        var hold = bodyWrap.getAttribute('hold');
-        if (hold) { this.hold = parseInt(hold, 10); }
-        var wait = bodyWrap.getAttribute('wait');
-        if (wait) { this.wait = parseInt(wait, 10); }
+            // check to make sure we don't overwrite these if _connect_cb is
+            // called multiple times in the case of missing stream:features
+            if (!this.sid) {
+                this.sid = bodyWrap.getAttribute("sid");
+            }
+            if (!this.stream_id) {
+                this.stream_id = bodyWrap.getAttribute("authid");
+            }
+            var wind = bodyWrap.getAttribute('requests');
+            if (wind) { this.window = parseInt(wind, 10); }
+            var hold = bodyWrap.getAttribute('hold');
+            if (hold) { this.hold = parseInt(hold, 10); }
+            var wait = bodyWrap.getAttribute('wait');
+            if (wait) { this.wait = parseInt(wait, 10); }
 
         this._authentication.sasl_scram_sha1 = false;
         this._authentication.sasl_plain = false;
