72be0a0ec4549f3f11fbd2998febfd30795780d0
Gordin <9ordin@googlemail.com>
2012-05-23 03:40:50 +0200

    use _onIdle from protocol object

--- src/core.js
+++ src/core.js
@@ -3260,12 +3260,7 @@ Strophe.Connection.prototype = {
 
         var body, time_elapsed;
 
-
-        if (this.protocol === Strophe.ProtocolType.WEBSOCKET) {
-            this._websocket_onIdle_helper();
-        } else {
-            this._bosh_onIdle_helper();
-        }
+        this.po._onIdle();
 
         clearTimeout(this._idleTimeout);
 
@@ -3273,8 +3268,7 @@ Strophe.Connection.prototype = {
         if (this.connected) {
             this._idleTimeout = setTimeout(this._onIdle.bind(this), 100);
         }
-    },
-
+    }
 };
 
 Strophe.Bosh = function(connection) {
