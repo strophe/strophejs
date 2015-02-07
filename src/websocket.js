b4aeec57a6a4f3b416035cd2570c2b77a841938e
JC Brand <jc@opkode.com>
2015-02-07 14:39:30 +0100

    Bugfix. $build was not defined in websocket.js

--- src/websocket.js
+++ src/websocket.js
@@ -11,14 +11,17 @@
 (function (root, factory) {
     if (typeof define === 'function' && define.amd) {
         // AMD. Register as an anonymous module.
-        define(['strophe-core'], function (wrapper) {
-            return factory(wrapper.Strophe);
+        define(['strophe-core'], function (core) {
+            return factory(
+                core.Strophe,
+                core.$build
+            );
         });
     } else {
         // Browser globals
-        return factory(Strophe);
+        return factory(Strophe, $build);
     }
-}(this, function (Strophe) {
+}(this, function (Strophe, $build) {
 
 /** Class: Strophe.WebSocket
  *  _Private_ helper class that handles WebSocket Connections
