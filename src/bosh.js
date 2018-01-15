097555ed92bdd110db3686bf12f39b61d49ac550
Paul "Joey" Clark <joey@neuralyte.org>
2018-01-15 20:12:03 +0800

    Fix for React-Native: parse responseText if responseXML is missing (#281)
    * Fix for React-Native: parse responseText if responseXML is missing
    
    * If parser produced nothing, treat that as an error

--- src/bosh.js
+++ src/bosh.js
@@ -97,9 +97,18 @@ Strophe.Request.prototype = {
                 throw "parsererror";
             }
         } else if (this.xhr.responseText) {
-            Strophe.error("invalid response received");
-            Strophe.error("responseText: " + this.xhr.responseText);
-            throw "badformat";
+            // In React Native, we may get responseText but no responseXML.  We can try to parse it manually.
+            Strophe.debug("Got responseText but no responseXML; attempting to parse it with DOMParser...");
+            try {
+                node = new DOMParser().parseFromString(this.xhr.responseText, 'application/xml').documentElement;
+                if (!node) {
+                    throw new Error('Parsing produced null node');
+                }
+            } catch (e) {
+                Strophe.error("invalid response received: " + e);
+                Strophe.error("responseText: " + this.xhr.responseText);
+                throw "badformat";
+            }
         }
 
         return node;
