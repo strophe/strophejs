5214319ed2dee6125f00956f5da454f2617c7f1e
Leonardo Donelli <learts92@gmail.com>
2019-03-31 00:12:33 +0100

    Make tests run correctly

--- tests/main.js
+++ tests/main.js
@@ -7,8 +7,8 @@ require.config({
         "basic":            "examples/basic",
 
         // Not really used, only to make requirejs not complain in tests
-        "xmldom":           "node_modules/xmldom/dom-parser",
-        "ws":               "node_modules/ws/index",
+        "ws":               "node_modules/ws/browser",
+        "xhr2":             "node_modules/xhr2/lib/browser",
 
         // Tests
         "jquery":           "node_modules/jquery/dist/jquery",
@@ -16,7 +16,15 @@ require.config({
         "sinon-qunit":      "node_modules/sinon-qunit/lib/sinon-qunit",
         "tests":            "tests/tests"
     },
-
+    map: {
+        // xmldom doesn't correctly load under AMD / requirejs
+        // map it to load 'ws' instead just to avoid the require error.
+        // It doesn't matter that it's a different module, as both of them
+        // are not actually used in the browser.
+        '*': {
+            'xmldom': 'ws'
+        }
+    },
     shim: {
         'sinon-qunit':  { deps: ['sinon']}
     }
