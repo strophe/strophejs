84f1091d8380fc721b8d3bd963c8f20a6347dc3f
Leonardo Donelli <learts92@gmail.com>
2019-05-26 02:10:18 +0200

    Complete migration to rollup

--- rollup.config.js
+++ rollup.config.js
@@ -1,6 +1,8 @@
 import resolve from 'rollup-plugin-node-resolve';
 import commonjs from 'rollup-plugin-commonjs';
 import babel from 'rollup-plugin-babel';
+import globals from 'rollup-plugin-node-globals';
+import { uglify } from 'rollup-plugin-uglify';
 import pkg from './package.json';
 
 export default [
@@ -15,9 +17,42 @@ export default [
 		plugins: [
 			resolve(),
 			commonjs(),
+            globals(),
 			babel({
-				exclude: ['node_modules/**']
+				exclude: ['node_modules/**'],
+                presets: [
+                    ['@babel/preset-env', {
+                        targets: {
+                            browsers: '>1%'
+                        }
+                    }]
+                ]
 			})
+		]
+	},
+	// Minified UMD build
+	{
+		input: 'src/strophe.js',
+		output: {
+			name: 'strophe',
+			file: 'dist/strophe.umd.min.js',
+			format: 'umd'
+		},
+		plugins: [
+			resolve(),
+			commonjs(),
+            globals(),
+			babel({
+                exclude: ['node_modules/**'],
+                presets: [
+                    ['@babel/preset-env', {
+                        targets: {
+                            browsers: '>1%'
+                        }
+                    }]
+                ]
+            }),
+            uglify()
 		]
 	},
 	// CommonJS (for Node) and ES module (for bundlers) build.
@@ -29,8 +64,12 @@ export default [
 			{ file: pkg.module, format: 'es' }
 		],
 		plugins: [
+            globals(),
 			babel({
-				exclude: ['node_modules/**']
+				exclude: ['node_modules/**'],
+                presets: [
+                    ['@babel/preset-env']
+                ]
 			})
 		]
 	}
