64666d45eb9f936a0441c191b6b2f28119961972
mightymop <totzkotz@gmail.com>
2021-01-10 11:40:40 +0100

    fix #369 reset SHA-1 challenge method

--- src/core.js
+++ src/core.js
@@ -1602,6 +1602,7 @@ Strophe.Connection = class Connection {
         // SASL
         this.do_session = false;
         this.do_bind = false;
+        this.mechanisms["SCRAM-SHA-1"].reset();
 
         // handler lists
         this.timedHandlers = [];
@@ -3355,78 +3356,81 @@ Strophe.SASLSHA1 = class SASLSHA1 extends Strophe.SASLMechanism {
      */
     constructor (mechname='SCRAM-SHA-1', isClientFirst=true, priority=60) {
         super(mechname, isClientFirst, priority);
+        this.reset();
     }
 
     test (connection) { // eslint-disable-line class-methods-use-this
         return connection.authcid !== null;
     }
 
-    onChallenge (connection, challenge, test_cnonce) {
-        const cnonce = test_cnonce || MD5.hexdigest("" + (Math.random() * 1234567890));
-        let auth_str = "n=" + utils.utf16to8(connection.authcid);
-        auth_str += ",r=";
-        auth_str += cnonce;
-        connection._sasl_data.cnonce = cnonce;
-        connection._sasl_data["client-first-message-bare"] = auth_str;
-        auth_str = "n,," + auth_str;
+    reset() {
+        this.onChallenge = (connection, challenge, test_cnonce) => {
+            const cnonce = test_cnonce || MD5.hexdigest("" + (Math.random() * 1234567890));
+            let auth_str = "n=" + utils.utf16to8(connection.authcid);
+            auth_str += ",r=";
+            auth_str += cnonce;
+            connection._sasl_data.cnonce = cnonce;
+            connection._sasl_data["client-first-message-bare"] = auth_str;
+            auth_str = "n,," + auth_str;
 
-        this.onChallenge = (connection, challenge) => {
-            let nonce, salt, iter, Hi, U, U_old, i, k;
-            let responseText = "c=biws,";
-            let authMessage = `${connection._sasl_data["client-first-message-bare"]},${challenge},`;
-            const cnonce = connection._sasl_data.cnonce;
-            const attribMatch = /([a-z]+)=([^,]+)(,|$)/;
+            this.onChallenge = (connection, challenge) => {
+                let nonce, salt, iter, Hi, U, U_old, i, k;
+                let responseText = "c=biws,";
+                let authMessage = `${connection._sasl_data["client-first-message-bare"]},${challenge},`;
+                const cnonce = connection._sasl_data.cnonce;
+                const attribMatch = /([a-z]+)=([^,]+)(,|$)/;
 
-            while (challenge.match(attribMatch)) {
-                const matches = challenge.match(attribMatch);
-                challenge = challenge.replace(matches[0], "");
-                switch (matches[1]) {
-                case "r":
-                    nonce = matches[2];
-                    break;
-                case "s":
-                    salt = matches[2];
-                    break;
-                case "i":
-                    iter = matches[2];
-                    break;
+                while (challenge.match(attribMatch)) {
+                    const matches = challenge.match(attribMatch);
+                    challenge = challenge.replace(matches[0], "");
+                    switch (matches[1]) {
+                    case "r":
+                        nonce = matches[2];
+                        break;
+                    case "s":
+                        salt = matches[2];
+                        break;
+                    case "i":
+                        iter = matches[2];
+                        break;
+                    }
                 }
-            }
 
-            if (nonce.substr(0, cnonce.length) !== cnonce) {
-                connection._sasl_data = {};
-                return connection._sasl_failure_cb();
-            }
+                if (nonce.substr(0, cnonce.length) !== cnonce) {
+                    connection._sasl_data = {};
+                    return connection._sasl_failure_cb();
+                }
 
-            responseText += "r=" + nonce;
-            authMessage += responseText;
+                responseText += "r=" + nonce;
+                authMessage += responseText;
 
-            salt = atob(salt);
-            salt += "\x00\x00\x00\x01";
+                salt = atob(salt);
+                salt += "\x00\x00\x00\x01";
 
-            const pass = utils.utf16to8(connection.pass);
-            Hi = U_old = SHA1.core_hmac_sha1(pass, salt);
-            for (i=1; i<iter; i++) {
-                U = SHA1.core_hmac_sha1(pass, SHA1.binb2str(U_old));
+                const pass = utils.utf16to8(connection.pass);
+                Hi = U_old = SHA1.core_hmac_sha1(pass, salt);
+                for (i=1; i<iter; i++) {
+                    U = SHA1.core_hmac_sha1(pass, SHA1.binb2str(U_old));
+                    for (k = 0; k < 5; k++) {
+                        Hi[k] ^= U[k];
+                    }
+                    U_old = U;
+                }
+                Hi = SHA1.binb2str(Hi);
+
+                const clientKey = SHA1.core_hmac_sha1(Hi, "Client Key");
+                const serverKey = SHA1.str_hmac_sha1(Hi, "Server Key");
+                const clientSignature = SHA1.core_hmac_sha1(SHA1.str_sha1(SHA1.binb2str(clientKey)), authMessage);
+                connection._sasl_data["server-signature"] = SHA1.b64_hmac_sha1(serverKey, authMessage);
+
                 for (k = 0; k < 5; k++) {
-                    Hi[k] ^= U[k];
+                    clientKey[k] ^= clientSignature[k];
                 }
-                U_old = U;
+                responseText += ",p=" + btoa(SHA1.binb2str(clientKey));
+                return responseText;
             }
-            Hi = SHA1.binb2str(Hi);
-
-            const clientKey = SHA1.core_hmac_sha1(Hi, "Client Key");
-            const serverKey = SHA1.str_hmac_sha1(Hi, "Server Key");
-            const clientSignature = SHA1.core_hmac_sha1(SHA1.str_sha1(SHA1.binb2str(clientKey)), authMessage);
-            connection._sasl_data["server-signature"] = SHA1.b64_hmac_sha1(serverKey, authMessage);
-
-            for (k = 0; k < 5; k++) {
-                clientKey[k] ^= clientSignature[k];
-            }
-            responseText += ",p=" + btoa(SHA1.binb2str(clientKey));
-            return responseText;
+            return auth_str;
         }
-        return auth_str;
     }
 }
 
