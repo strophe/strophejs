5b9370aa440138e47d830bdbfdf3429e2db9f05a
JC Brand <jc@opkode.com>
2023-06-13 11:21:28 +0200

    Add ability to test Strophe in NodeJS
    - Update to Node 19 so that `crypto` is available globally
    - Fix failing tests due to differences between DOMParser in the browser and Node.
    - Use `globalThis` where appropriate.

--- src/strophe.js
+++ src/strophe.js
@@ -4,11 +4,13 @@ import './bosh';
 import './websocket';
 import './worker-websocket';
 import * as strophe from './core';
+import * as shims from './shims';
 
 global.$build = strophe.default.$build;
 global.$iq = strophe.default.$iq;
 global.$msg = strophe.default.$msg;
 global.$pres = strophe.default.$pres;
 global.Strophe = strophe.default.Strophe;
+global.Strophe.shims = shims;
 
 export { Strophe, $build, $iq, $msg, $pres } from './core';
